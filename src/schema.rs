b1e426e3310371c55e11bfffe8c1cfe2d23d2408
Mrmaxmeier <Mrmaxmeier@gmail.com>
2017-02-11 17:17:38 +0100

    add db schema to repo

--- src/schema.rs
+++ src/schema.rs
@@ -1,1 +1,13 @@
-infer_schema!("dotenv:DATABASE_URL");
+// infer_schema!("dotenv:DATABASE_URL");
+table! {
+    users (id) {
+        id -> Int4,
+        username -> Varchar,
+        email -> Varchar,
+        firstname -> Nullable<Varchar>,
+        lastname -> Nullable<Varchar>,
+        pwhash -> Text,
+        admin -> Bool,
+        name_public -> Bool,
+    }
+}
