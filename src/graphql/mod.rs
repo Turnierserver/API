8ff3c17577750cfe1ecdfee06beb50058ba92b8e
Mrmaxmeier <Mrmaxmeier@gmail.com>
2017-03-05 18:37:46 +0100

    remove snake_oil, return null on invalid auth

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -6,9 +6,22 @@ pub use self::query::*;
 mod mutation;
 pub use self::mutation::*;
 
+use std::str;
 use juniper::ID;
-use base64::encode;
+use base64;
 
 pub fn id(pre: &str, index: i32) -> ID {
-    ID::from(encode(format!("{}-{}", pre, index).as_bytes()))
+    let s = format!("{}-{}", pre, index);
+    ID::from(base64::encode(s.as_bytes()))
+}
+
+pub fn rid(pre: &str, id: &ID) -> Result<i32, String> {
+    let s = base64::decode(&*id).map_err(|_| "invalid base64".to_owned())?;
+    let s = str::from_utf8(&s).map_err(|_| "invalid kind str".to_owned())?;
+    let (kind, data) = s.split_at(pre.len() + 1);
+    if &kind[0..kind.len() - 1] != pre {
+        Err(format!("wrong kind, expected {}", pre))
+    } else {
+        data.parse().map_err(|_| "expected number".to_owned())
+    }
 }
--- src/graphql/mutation.rs
+++ src/graphql/mutation.rs
@@ -7,43 +7,38 @@ use models::*;
 
 pub struct Mutation;
 graphql_object!(Mutation: Context as "Mutation" |&self| {
-    field test_mutate(new_val: i64) -> FieldResult<bool> {
-        Ok(new_val > 1)
-    }
-
-    field snake_oil(input: ()) -> SnakeOil { SnakeOil }
-
     field auth(&executor, input: AuthCredentials) -> FieldResult<AuthOutput> {
-        let user = User::named(input.username, &executor.context().conn)
-            .map_err(|_| "unauthorized".to_owned())?;
-
-        if user.verify_pass(&*input.password) {
-            let token = executor.context().try(|conn| user.token(conn).map(
-                |token| token.hyphenated().to_string()
-            ))?;
-            executor.context().set_token_cookie(&*token);
-            Ok(AuthOutput {
-                token: token,
-                client_mutation_id: input.client_mutation_id,
-                user: user
-            })
-        } else {
-            println!("invalid password");
-            Err("unauthorized".to_owned())
+        if let Ok(user) = User::named(input.username, &executor.context().conn) {
+            if user.verify_pass(&*input.password) {
+                let token = executor.context().try(|conn| user.token(conn).map(
+                    |token| token.hyphenated().to_string()
+                ))?;
+                executor.context().set_token_cookie(&*token);
+                return Ok(AuthOutput {
+                    client_mutation_id: input.client_mutation_id,
+                    token: Some(token),
+                    user: Some(user)
+                })
+            }
         }
+        Ok(AuthOutput {
+            client_mutation_id: input.client_mutation_id,
+            token: None,
+            user: None
+        })
     }
 });
 
 #[derive(Debug)]
 pub struct AuthOutput {
     client_mutation_id: String,
-    token: String,
-    user: User
+    token: Option<String>,
+    user: Option<User>
 }
 graphql_object!(AuthOutput: Context as "AuthOutput" |&self| {
     field clientMutationId() -> &String { &self.client_mutation_id }
-    field token() -> &String { &self.token }
-    field user() -> &User { &self.user }
+    field token() -> &Option<String> { &self.token }
+    field user() -> &Option<User> { &self.user }
 });
 
 
@@ -54,10 +49,3 @@ graphql_input_object!(
         password: String,
     }
 );
-
-// TODO
-graphql_input_object!(struct SnakeOilInput {
-    placebo: i64
-});
-struct SnakeOil;
-graphql_object!(SnakeOil: Context |&self| {});
